dc5627d9028a87a8a91a689d2152d5241237a298
sstern6 <scott.stern06@gmail.com>
2015-09-25 15:05:35 -0700

    Login and signup working without all validations

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -8,6 +8,15 @@ end
 
 post '/login' do
 
+  @user = User.find_by(:username => params[:username])
+  if User.authenticate(params[:username],params[:password])
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    # @login_errors = @user.errors.full_messages
+    # erb :'/login'
+    redirect '/login'
+  end
 
 end
 
@@ -29,12 +38,14 @@ end
 post '/users/new' do
 
   @new_user = User.new(params[:user])
+  p @new_user.id
 
   if @new_user.save
     session[:user_id] = @new_user.id
     redirect "/users/#{@new_user.id}"
   else
-    p @signup_errors = @new_user.errors.full_messages
+    @signup_errors = @new_user.errors.full_messages
+    erb :'/signup/index'
   end
 
 end
